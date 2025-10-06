396fea1d9528073bac4bdcfbb4b0b37683eb661b
John Mintz <john.mintz@gmail.com>
2025-10-06 16:32:28 -0500

    Second Commit

---
+++ another.java
@@ -0,0 +1,9 @@
+//This is the second java file I am buidling
+public class another {
+  public static void main(String[] args) {
+    
+    System.out.println("Java is a class-based language.");
+    System.out.println("This is another attempt at Java.");
+
+      }
+}
