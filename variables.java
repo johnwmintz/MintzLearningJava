6434526560d5b57dcf226bf85a785f1ef3a50c92
John Mintz <john.mintz@gmail.com>
2025-10-06 19:29:00 -0500

    Variable Practice
    Practice with Variables.

---
+++ variables.java
@@ -0,0 +1,16 @@
+//This is code for me to practice variables.
+public class variables {
+    public static void main (String[] args) {
+        //Declaring variables
+        int justNumber = 47258;
+        char grade = 'A';
+        boolean trueOrFalse = false;
+        String lotsOfWords = "This is alot of words";
+
+        //print out the variables
+        System.out.println (justNumber);
+        System.out.println (grade);
+        System.out.println (trueOrFalse);
+        System.out.println (lotsOfWords);
+    }
+}
