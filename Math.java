d1aa065e5055a56f3c18faf2bc7ddfd360ad8a04
John Mintz <john.mintz@gmail.com>
2025-12-14 15:42:20 -0600

    adding math practice

---
+++ Math.java
@@ -0,0 +1,34 @@
+//This is practice for math with int and double
+public class Math {
+    public static void main(String[] args) {
+    //practice with int math
+    int x = 10;
+    int y = 3;
+    System.out.println(x + y); // 13
+    System.out.println(x - y); // 7
+    System.out.println(x * y); // 30
+    System.out.println(x / y); // 3
+    System.out.println(x % y); // 1
+    int z = 5;
+    ++z;
+    System.out.println(z); // 6
+    --z;
+    System.out.println(z); // 5
+    //practice with double math
+    double a = 10.0;
+    double b = 3.0;
+    System.out.println(a + b); 
+    System.out.println(a - b);
+    System.out.println(a * b); 
+    System.out.println(a / b); 
+    System.out.println(a % b);
+    double c = 7.0;
+    ++c;
+    System.out.println(c);
+    --c;
+    System.out.println(c); 
+
+
+    }
+
+}
