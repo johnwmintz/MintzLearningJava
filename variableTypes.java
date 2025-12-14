91404aec818f94aab2729d3fe38f9fb5aef9cc2f
John Mintz <john.mintz@gmail.com>
2025-12-14 15:00:08 -0600

    Updating practice on variable type

---
+++ variableTypes.java
@@ -0,0 +1,40 @@
+//This is practice for some variables
+public class variableTypes {
+    public static void main(String[] args) {
+    //Boolean
+    int x = 10;
+    int y = 11;
+    System.out.println(x==10);
+    System.out.println(y==4);
+    //byte -128 to 127
+    byte num1 = -25;
+    byte num2 = 125;
+    System.out.println (num1);
+    System.out.println (num2);
+    //short -32,768 to 32,767
+    short num3 = -31956;
+    short num4 = 32555;
+    System.out.println (num3);
+    System.out.println (num4);
+    //int -2,147,483,468 to 2,147,483,647
+    int num5 = -2122888765;
+    int num6 = 2135476527;
+    System.out.println (num5);
+    System.out.println (num6);
+    //Char = single letter
+    char letter1 = 'D';
+    char letter2 = 'f';
+    System.out.println (letter1);
+    System.out.println (letter2);
+    //String = words....
+    String words = "This is a string";
+    System.out.println (words);
+
+
+
+
+
+    
+
+      }
+}
