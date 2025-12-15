7fe25b470f2d720fed861ad9dad4c20db3d96ae9
John Mintz <john.mintz@gmail.com>
2025-12-14 19:44:52 -0600

    Adding Logical and Compare

---
+++ Compare.java
@@ -0,0 +1,16 @@
+//This is to compare numbers
+public class Compare {
+    public static void main(String[] args) {    
+        int x = (10);
+        int y = (5);
+
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x > y);
+        System.out.println(x < y);
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+
+}
+
+}
---
+++ Logical.java
@@ -0,0 +1,13 @@
+//This is to test logical operators
+public class Logical {
+    public static void main(String[] args) {    
+        boolean isLoggedIn = true;
+        boolean isAdmin = false;
+
+        System.out.println("Regular user: " + (isLoggedIn && !isAdmin));
+        System.out.println("Has access: " + (isLoggedIn || isAdmin));
+        System.out.println("Not logged in: " + (!isLoggedIn));
+
+}
+
+}
