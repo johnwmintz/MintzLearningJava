02ca9bd0241afb1ed3a24751a487d93c031c79a1
John Mintz <john.mintz@gmail.com>
2025-12-15 20:01:11 -0600

    if and else practice

---
+++ ifandelse.java
@@ -0,0 +1,12 @@
+public class ifandelse {
+    public static void main(String[] args) {
+    boolean isRaining = true;
+    if (isRaining) {
+        System.out.println ("Bring an Umbrella");
+    }
+    else {
+        System.out.println ("No rain today no need for umbrella");
+    }
+     }
+
+}
