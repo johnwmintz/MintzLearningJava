ccf74f46695bf6a1ac2441ea83c5c37d5b3ead8a
John Mintz <john.mintz@gmail.com>
2025-12-15 20:16:41 -0600

    adding elseif

---
+++ elseif.java
@@ -0,0 +1,13 @@
+//This is to practice elseif
+public class elseif {
+    public static void main(String[] args) {
+        int weather = 1; // 1 = raining, 2 = sunny, 3 = cloudy
+        if (weather == 1) {
+            System.out.println("Bring an umbrella.");
+        }else if (weather == 2) {
+            System.out.println("Wear sunglasses.");
+        } else {
+            System.out.println("Just go outside normally.");
+        }
+    }
+}
