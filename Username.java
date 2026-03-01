1765824e2ec29234b5010c887e746927f215080c
John Mintz <john.mintz@gmail.com>
2026-03-01 10:21:54 -0600

    More practice

---
+++ Username.java
@@ -0,0 +1,10 @@
+public class Username {
+    public static void main(String[] args) {    
+      String username1 = "teracocer";
+      String username2 = "gigacoder";
+
+      System.out.println(username1.equals(username2));
+
+
+}
+}
---
+++ another2.java
@@ -0,0 +1,10 @@
+//This is the second java file I am buidling
+public class another2 {
+  public static void main(String[] args) {
+    
+    System.out.println("Java is a class-based language.");
+    System.out.println("This is another attempt at Java.");
+    System.out.println("I hope this works on a PC");
+
+      }
+}
